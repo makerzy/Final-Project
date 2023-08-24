3bac3e16ea9e994305f69fdeda88f42f9b3f33d1
alpnelson <alpnelson5@gmail.com>
2023-08-24 15:06:32 -0400

    updating the controllers

--- src/main/java/com/company/gamestore/controller/ConsoleController.java
+++ src/main/java/com/company/gamestore/controller/ConsoleController.java
@@ -1,15 +1,65 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Console;
+import com.company.gamestore.repository.ConsoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ConsoleController {
+    @Autowired
+    ConsoleRepository ConsoleRepo;
+
+
 
     //create console
+    @PostMapping(value = "/console")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Console createConsole(@RequestBody Console console) {
+        return ConsoleRepo.save(console);
+    }
 
 
     //read console
+    @GetMapping("/console/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Console getConsoleById(@PathVariable int id) {
+        Optional<Console> console = ConsoleRepo.findById(id);
+        if (console.isPresent())
+            return console.get();
+        return null;
+    }
+
+
 
 
     // update console
+    @PutMapping("/console/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console, @PathVariable Integer id) {
+        Optional<Console> console1 = ConsoleRepo.findById(id);
+        if (console1.isPresent())
+            ConsoleRepo.save(console);
+
+    }
 
 
     //delete console
+    @DeleteMapping("/console/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable Integer id) {
+        ConsoleRepo.deleteById(id);
+    }
+
+
+    //search  console by manufacturer
+    @GetMapping("/console/manufacturer/{manufacturer}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Console> getConsoleByManufacturer(@PathVariable String manufacturer) {
+        return ConsoleRepo.findByManufacturer(manufacturer);
+    }
 }
--- src/main/java/com/company/gamestore/controller/GameController.java
+++ src/main/java/com/company/gamestore/controller/GameController.java
@@ -4,9 +4,21 @@ public class GameController {
 
     //create new game
 
+
     //read games
+
 
     //update game
 
+
     //delete game
+
+
+    //search games by studio
+
+
+    //search games by ESRB rating
+
+
+    //search games by title
 }
