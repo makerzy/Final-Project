b5cd4173ab83d9ad93b131cf4dc51386127c782e
makerzy <botassammzy@gmail.com>
2023-08-26 06:23:24 -0700

    added console and game

--- src/main/java/com/company/gamestore/model/Console.java
+++ src/main/java/com/company/gamestore/model/Console.java
@@ -1,4 +1,24 @@
 package com.company.gamestore.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "console")
 public class Console {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int consoleId;
+    private String model;
+    private String manufacturer;
+    private String memoryAmount;
+    private String processor;
+    private double price;
+    private int quantity;
+
 }
--- src/main/java/com/company/gamestore/model/Game.java
+++ src/main/java/com/company/gamestore/model/Game.java
@@ -1,4 +1,21 @@
 package com.company.gamestore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "game")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int game_id;
+    private String esrbRating;
+    private String description;
+    private double price;
+    private String studio;
+    private int quantity;
 }
