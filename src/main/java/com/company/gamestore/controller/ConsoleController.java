4e264f9926c917e43426c23b6433e0b4da83a2de
alpnelson <alpnelson5@gmail.com>
2023-08-26 19:27:36 -0400

    dsada

--- src/main/java/com/company/gamestore/controller/ConsoleController.java
+++ src/main/java/com/company/gamestore/controller/ConsoleController.java
@@ -1,4 +1,66 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Console;
+import com.company.gamestore.repository.ConsoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ConsoleController {
-}
+    @Autowired
+    ConsoleRepository ConRepo;
+
+
+    // read console
+    @GetMapping("/console")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Console> getAllConsole() {
+        return ConRepo.findAll();
+    }
+
+
+
+    // update console
+    @PutMapping("/console/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console, @PathVariable Integer id) {
+        Optional<Console> console1 = ConRepo.findById(id);
+        if (console1.isPresent())
+            ConRepo.save(console);
+
+    }
+
+
+
+    //  create console
+    @PostMapping(value = "/console")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Console addConsole(@RequestBody Console console) {
+        return ConRepo.save(console);
+    }
+
+
+
+    // delete console
+    @DeleteMapping("/console/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable Integer id) {
+        ConRepo.deleteById(id);
+    }
+
+
+
+
+    // search console by manufacturer
+
+    @GetMapping("/console/manufacturer/{manufacturer}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Console> getConsoleByManufacturer(@PathVariable String manufacturer) {
+        return ConRepo.findByManufacturer(manufacturer);
+    }
+
+    }
--- src/main/java/com/company/gamestore/controller/GameController.java
+++ src/main/java/com/company/gamestore/controller/GameController.java
@@ -1,4 +1,66 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Console;
+import com.company.gamestore.model.Game;
+import com.company.gamestore.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class GameController {
+    @Autowired
+    GameRepository GameRepo;
+
+
+    // update game
+
+
+
+    // create game
+
+    @PostMapping(value = "/game")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game addGame(@RequestBody Game game) {
+        return GameRepo.save(game);
+    }
+
+
+
+
+    // read game
+    @GetMapping("/game")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getAllGame() {
+        return GameRepo.findAll();
+    }
+
+
+
+
+    // delete game
+    @DeleteMapping("/game/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGame(@PathVariable Integer id) {
+        GameRepo.deleteById(id);
+    }
+
+
+
+
+    // search game by studio
+    @GetMapping("/game/studio/{studio}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getGameByStudio(@PathVariable String studio) {
+        return GameRepo.findByStudio(studio);
+    }
+
+
+    // search game by esrb rating
+
+
+    // search game by title
+
 }
